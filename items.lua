a8724c64dac933bb8c918e6a2802774c504d6203
Daniel Hufler <mail@danielhufler.at>
2023-08-26 22:38:41 +0200

    Renamed files

--- items.lua
+++ items.lua
@@ -21,44 +21,44 @@ PlaceObj('ModItemCharacterEffectCompositeDef', {
 	'Description', T(579526836304, --[[ModItemCharacterEffectCompositeDef GCMilitia Description]] "A perk every militia solider gets after bootcamp. Which allows them to clean and maintain their weapons."),
 }),
 PlaceObj('ModItemCode', {
-	'name', "Finances",
-	'CodeFileName', "Code/Finances.lua",
+	'name', "MilitiaFinances",
+	'CodeFileName', "Code/MilitiaFinances.lua",
 }),
 PlaceObj('ModItemCode', {
-	'name', "Init",
-	'CodeFileName', "Code/Init.lua",
+	'name', "MilitiaInit",
+	'CodeFileName', "Code/MilitiaInit.lua",
 }),
 PlaceObj('ModItemCode', {
 	'name', "MilitiaManagement",
 	'CodeFileName', "Code/MilitiaManagement.lua",
 }),
 PlaceObj('ModItemCode', {
-	'name', "Movement",
-	'CodeFileName', "Code/Movement.lua",
+	'name', "MilitiaMovement",
+	'CodeFileName', "Code/MilitiaMovement.lua",
 }),
 PlaceObj('ModItemCode', {
-	'name', "PerkEffects",
-	'CodeFileName', "Code/PerkEffects.lua",
+	'name', "MilitiaPerkEffects",
+	'CodeFileName', "Code/MilitiaPerkEffects.lua",
 }),
 PlaceObj('ModItemCode', {
-	'name', "Personalization",
-	'CodeFileName', "Code/Personalization.lua",
+	'name', "MilitiaPersonalization",
+	'CodeFileName', "Code/MilitiaPersonalization.lua",
 }),
 PlaceObj('ModItemCode', {
-	'name', "SatelliteConflict",
-	'CodeFileName', "Code/SatelliteConflict.lua",
+	'name', "MilitiaSatelliteConflict",
+	'CodeFileName', "Code/MilitiaSatelliteConflict.lua",
 }),
 PlaceObj('ModItemCode', {
-	'name', "BiosAndNames",
-	'CodeFileName', "Code/BiosAndNames.lua",
+	'name', "MilitiaBiosAndNames",
+	'CodeFileName', "Code/MilitiaBiosAndNames.lua",
 }),
 PlaceObj('ModItemCode', {
-	'name', "SquadsAndMilitia",
-	'CodeFileName', "Code/SquadsAndMilitia.lua",
+	'name', "MilitiaAndSquads",
+	'CodeFileName', "Code/MilitiaAndSquads.lua",
 }),
 PlaceObj('ModItemCode', {
-	'name', "Training",
-	'CodeFileName', "Code/Training.lua",
+	'name', "MilitiaTraining",
+	'CodeFileName', "Code/MilitiaTraining.lua",
 }),
 PlaceObj('ModItemCode', {
 	'name', "Utils",
