39cc96fb958aebe29b2052a0ed178219e9eb63ca
Daniel Hufler <mail@danielhufler.at>
2023-09-22 22:02:06 +0200

    Moved presets to preset folder

---
+++ presets/1_MilitiaShopCategories.lua
@@ -0,0 +1,135 @@
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "pistols",
+    title = "Pistols",
+    description = "Discover a selection of top-quality handguns.",
+    weight = 100,
+    order = 10
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "smgs",
+    title = "SMGs",
+    description = "Explore the best submachine guns available.",
+    weight = 200,
+    order = 20
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "assault",
+    title = "Assault Rifles",
+    description = "Find high-performance assault rifles for your missions.",
+    weight = 300,
+    order = 30
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "shotguns",
+    title = "Shotguns",
+    description = "Select from top-notch shotguns designed for combat.",
+    weight = 200,
+    order = 40
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "rifles",
+    title = "Rifles & Snipers",
+    description = "Browse precision rifles and powerful sniper rifles.",
+    weight = 400,
+    order = 50
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "machineguns",
+    title = "Machine Guns",
+    description = "Explore heavy-duty machine guns built for battle.",
+    weight = 500,
+    order = 70
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "heavy",
+    title = "Heavy Weapons",
+    description = "Find the most formidable heavy weapons on the market.",
+    weight = 500,
+    order = 80
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "melee",
+    title = "Melee Weapons",
+    description = "Melee weapons for close-quarters combat.",
+    weight = 50,
+    order = 90
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "ammo",
+    title = "Ammo",
+    description = "Stock up on high-quality ammunition for your firearms.",
+    weight = 1,
+    order = 100
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "explosives",
+    title = "Explosives",
+    description = "Explosives designed for tactical advantage.",
+    weight = 100,
+    order = 110
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "armor",
+    title = "Armor",
+    description = "Protective gear to keep you safe in the field.",
+    weight = 200,
+    order = 120
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "tools",
+    title = "Tools",
+    description = "Essential tools and equipment for your missions.",
+    weight = 100,
+    order = 130
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "misc",
+    title = "Misc",
+    description = "Assorted miscellaneous items for various needs.",
+    weight = 100,
+    order = 140
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "crafting",
+    title = "Crafting",
+    description = "Crafting materials and resources for skilled artisans.",
+    weight = 50,
+    order = 150
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "medical",
+    title = "Medical",
+    description = "Medical supplies and first-aid essentials.",
+    weight = 50,
+    order = 160
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "throwables",
+    title = "Grenades",
+    description = "An assortment of grenades and tactical throwables.",
+    weight = 50,
+    order = 170
+})
+
+PlaceObj('HUDAMilitiaShopCategory', {
+    id = "magazines",
+    title = "Magazines",
+    description = "Educational magazines to enhance your skills.",
+    weight = 50,
+    order = 180
+})
---
+++ presets/1_MilitiaShopCouponCodes.lua
@@ -0,0 +1,27 @@
+PlaceObj('HUDAMilitiaShopCouponCode', {
+    id = "huda0816",
+    discount = 50,
+    multi = false,
+    description = "50% off all Militia Shop items"
+})
+
+PlaceObj('HUDAMilitiaShopCouponCode', {
+    id = "jadiscord",
+    discount = 100,
+    multi = false,
+    description = "100% off all Militia Shop items"
+})
+
+PlaceObj('HUDAMilitiaShopCouponCode', {
+    id = "bark0816",
+    discount = 50,
+    multi = false,
+    description = "50% off all Militia Shop items"
+})
+
+PlaceObj('HUDAMilitiaShopCouponCode', {
+    id = "reopen30",
+    discount = 30,
+    multi = false,
+    description = "30% off all Militia Shop items"
+})
