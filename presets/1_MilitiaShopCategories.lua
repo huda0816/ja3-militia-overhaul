17776e10e639adc00671dee231e08f3e0531510d
Daniel Hufler <mail@danielhufler.at>
2023-09-23 19:39:32 +0200

    Made last adjustments for extendability

--- presets/1_MilitiaShopCategories.lua
+++ presets/1_MilitiaShopCategories.lua
@@ -2,7 +2,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "pistols",
     title = "Pistols",
     description = "Discover a selection of top-quality handguns.",
-    weight = 100,
+    weight = 1000,
     order = 10
 })
 
@@ -10,7 +10,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "smgs",
     title = "SMGs",
     description = "Explore the best submachine guns available.",
-    weight = 200,
+    weight = 2000,
     order = 20
 })
 
@@ -18,7 +18,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "assault",
     title = "Assault Rifles",
     description = "Find high-performance assault rifles for your missions.",
-    weight = 300,
+    weight = 3000,
     order = 30
 })
 
@@ -26,7 +26,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "shotguns",
     title = "Shotguns",
     description = "Select from top-notch shotguns designed for combat.",
-    weight = 200,
+    weight = 2000,
     order = 40
 })
 
@@ -34,7 +34,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "rifles",
     title = "Rifles & Snipers",
     description = "Browse precision rifles and powerful sniper rifles.",
-    weight = 400,
+    weight = 4000,
     order = 50
 })
 
@@ -42,7 +42,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "machineguns",
     title = "Machine Guns",
     description = "Explore heavy-duty machine guns built for battle.",
-    weight = 500,
+    weight = 5000,
     order = 70
 })
 
@@ -50,7 +50,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "heavy",
     title = "Heavy Weapons",
     description = "Find the most formidable heavy weapons on the market.",
-    weight = 500,
+    weight = 5000,
     order = 80
 })
 
@@ -58,7 +58,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "melee",
     title = "Melee Weapons",
     description = "Melee weapons for close-quarters combat.",
-    weight = 50,
+    weight = 500,
     order = 90
 })
 
@@ -66,7 +66,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "ammo",
     title = "Ammo",
     description = "Stock up on high-quality ammunition for your firearms.",
-    weight = 1,
+    weight = 10,
     order = 100
 })
 
@@ -74,7 +74,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "explosives",
     title = "Explosives",
     description = "Explosives designed for tactical advantage.",
-    weight = 100,
+    weight = 1000,
     order = 110
 })
 
@@ -82,7 +82,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "armor",
     title = "Armor",
     description = "Protective gear to keep you safe in the field.",
-    weight = 200,
+    weight = 2000,
     order = 120
 })
 
@@ -90,7 +90,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "tools",
     title = "Tools",
     description = "Essential tools and equipment for your missions.",
-    weight = 100,
+    weight = 1000,
     order = 130
 })
 
@@ -98,7 +98,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "misc",
     title = "Misc",
     description = "Assorted miscellaneous items for various needs.",
-    weight = 100,
+    weight = 1000,
     order = 140
 })
 
@@ -106,7 +106,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "crafting",
     title = "Crafting",
     description = "Crafting materials and resources for skilled artisans.",
-    weight = 50,
+    weight = 500,
     order = 150
 })
 
@@ -114,7 +114,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "medical",
     title = "Medical",
     description = "Medical supplies and first-aid essentials.",
-    weight = 50,
+    weight = 500,
     order = 160
 })
 
@@ -122,7 +122,7 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "throwables",
     title = "Grenades",
     description = "An assortment of grenades and tactical throwables.",
-    weight = 50,
+    weight = 500,
     order = 170
 })
 
@@ -130,6 +130,6 @@ PlaceObj('HUDAMilitiaShopCategory', {
     id = "magazines",
     title = "Magazines",
     description = "Educational magazines to enhance your skills.",
-    weight = 50,
+    weight = 500,
     order = 180
 })
