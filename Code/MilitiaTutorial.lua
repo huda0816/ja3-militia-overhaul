1fd49e0af7417f6c54a4ddd639fcfa7eb2090564
Daniel Hufler <mail@danielhufler.at>
2023-09-26 22:06:36 +0200

    Removed unused function

--- Code/MilitiaTutorial.lua
+++ Code/MilitiaTutorial.lua
@@ -24,51 +24,6 @@ function ShowToggleMilitiaTutorial()
     end)
 end
 
-function HUDAOpenTutorialPopupSatelliteMap(onWindow, parent, preset, textContext)
-    if not preset then
-      return false
-    end
-    local enabled_option = GetAccountStorageOptionValue("HintsEnabled")
-    if not enabled_option then
-      return false
-    end
-    if not CanShowTutorialPopup then
-      return
-    end
-    local state = TutorialHintsState.mode[preset.id]
-    if preset.id ~= "Aiming" and state == "dismissed" then
-      return false
-    end
-    if not parent then
-      parent = GetDialog("PDADialogSatellite")
-      parent = parent and parent:ResolveId("idTutorialPopup")
-      parent.DrawOnTop = true
-    end
-    local popup = XTemplateSpawn("TutorialPopup", parent, preset)
-    if onWindow then
-      popup:SetAnchor(onWindow.box)
-    end
-    popup.idTitle:SetText(T(630309726328, "TUTORIAL"))
-    popup:Open()
-    popup.preset = preset
-    popup.textContext = textContext
-    popup:UpdateText()
-    if IsKindOf(onWindow, "XMapRolloverable") then
-      onWindow:SetupMapSafeArea(popup)
-      popup:CreateThread("update-pos", function()
-        while popup.window_state ~= "destroying" do
-          onWindow:SetupMapSafeArea(popup)
-          popup:InvalidateLayout()
-          Sleep(16)
-        end
-      end)
-    end
-    popup:SetVisible(true)
-    CurrentTutorialPopup = popup
-    CanShowTutorialPopup = false
-    return popup
-  end
-
 PlaceObj('TutorialHint', {
     Comment = "Triggers after trained militia",
     Text = Untranslated("Use this button to toggle between Militia and Mercenary squads."),
